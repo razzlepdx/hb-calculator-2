02cb66f9ef68a28ff1b75709559e200e9712304a
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-04 18:00:50 -0800

    Cleans up some of the if statements.

--- calculator.py
+++ calculator.py
@@ -7,20 +7,26 @@ calculator program yourself in this file.
 from arithmetic import *
 
 
+# TODO: in return_valid_tokens, return empty lists instead of False
+# TODO: also update conditional in while loop to check for empty list, not False
+# TODO: In return_valid_tokens, do clean up with creating additional variable names
+#       for ex: (len(tokens)-1)
+
 # Your code goes here
 
-def print_instructions():
-    """"Prints instructions for the calculator."""
-    print "Welcome to the Calculator!"
-    print "Type \"+ [number] [number]\" to add."
-    print "Type \"- [number] [number]\" to subtract."
-    print "Type \"* [number] [number]\" to multiply."
-    print "Type \"/ [number] [number]\" to divide."
-    print "Type \"square [number]\" to square the number."
-    print "Type \"cube [number]\" to cube the number."
-    print "Type \"pow [number] [number]\" to raise the first number to the power of the second number."
-    print "Type \"% [number] [number]\" to get the remainer of the first number divided by the second number."
-    print "Type \"q\" to exit the Calculator."
+def prompt_instructions():
+    """"Asks user if they want to see instructions for the calculator."""
+    view_instructions = raw_input("Would you like to view instructions? Y/n ")
+    if view_instructions.lower() == "y":
+        print "Type \"+ [number] [number]\" to add."
+        print "Type \"- [number] [number]\" to subtract."
+        print "Type \"* [number] [number]\" to multiply."
+        print "Type \"/ [number] [number]\" to divide."
+        print "Type \"square [number]\" to square the number."
+        print "Type \"cube [number]\" to cube the number."
+        print "Type \"pow [number] [number]\" to raise the first number to the power of the second number."
+        print "Type \"% [number] [number]\" to get the remainer of the first number divided by the second number."
+        print "Type \"q\" to exit the Calculator."
 
 
 def return_valid_tokens(tokens):
@@ -37,49 +43,55 @@ def return_valid_tokens(tokens):
 
     if tokens[0] not in param_req:
         print "Sorry, I don't understand that operation.  Try again!"
+        prompt_instructions()
         return False
-    else:
-        if param_req[tokens[0]] != (len(tokens)-1):
-            print "Sorry. That wasn't the correct amount of numbers for {} operation.".format(tokens[0])
-            print "Expected {}".format(param_req[tokens[0]])
-            print "Try again."
+
+    if param_req[tokens[0]] != (len(tokens)-1):
+        print "Sorry. That wasn't the correct amount of numbers for {} operation.".format(tokens[0])
+        print "Expected {}".format(param_req[tokens[0]])
+        print "Try again."
+        prompt_instructions()
+        return False
+
+    valid_inputs = [tokens[0]]
+    for token in tokens[1:]:
+        try:
+            token = float(token)
+            valid_inputs.append(token)
+        except ValueError:
+            print "Sorry, that wasn't a number.  Try again!"
+            prompt_instructions()
             return False
-        valid_inputs = [tokens[0]]
-        for token in tokens[1:]:
-            try:
-                token = float(token)
-                valid_inputs.append(token)
-            except ValueError:
-                print "Sorry, that wasn't a number.  Try again!"
-                return False
 
-        return valid_inputs
+    return valid_inputs
 
-print_instructions()
+print "Welcome to the Calculator!"
+prompt_instructions()
 while True:
     user_input = raw_input("> ")
     tokens = user_input.split(" ")
     if tokens[0] == "q":
         break
+    
+    tokens = return_valid_tokens(tokens)
+    if tokens is False:
+        continue
+    if tokens[0] == "+":
+        print add(tokens[1], tokens[2])
+    elif tokens[0] == "-":
+        print subtract(tokens[1], tokens[2])
+    elif tokens[0] == "*":
+        print multiply(tokens[1], tokens[2])
+    elif tokens[0] == "/":
+        print divide(tokens[1], tokens[2])
+    elif tokens[0] == "square":
+        print square(tokens[1])
+    elif tokens[0] == "cube":
+        print cube(tokens[1])
+    elif tokens[0] == "pow":
+        print power(tokens[1], tokens[2])
+    elif tokens[0] == "%":
+        print mod(tokens[1], tokens[2])
     else:
-        tokens = return_valid_tokens(tokens)
-        if tokens is False:
-            continue
-        if tokens[0] == "+":
-            print add(tokens[1], tokens[2])
-        elif tokens[0] == "-":
-            print subtract(float(tokens[1]), float(tokens[2]))
-        elif tokens[0] == "*":
-            print multiply(float(tokens[1]), float(tokens[2]))
-        elif tokens[0] == "/":
-            print divide(float(tokens[1]), float(tokens[2]))
-        elif tokens[0] == "square":
-            print square(float(tokens[1]))
-        elif tokens[0] == "cube":
-            print cube(float(tokens[1]))
-        elif tokens[0] == "pow":
-            print power(float(tokens[1]), float(tokens[2]))
-        elif tokens[0] == "%":
-            print mod(float(tokens[1]), float(tokens[2]))
-        else:
-            print "Try again!  I don't recognize that math."
+        print "Try again!  I don't recognize that math."
+        prompt_instructions()
