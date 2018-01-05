38ebfa0c441a38945000f53835e7cd4f6f73defc
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-04 16:15:51 -0800

    Adds user instructions.

--- calculator.py
+++ calculator.py
@@ -8,11 +8,23 @@ from arithmetic import *
 
 
 # Your code goes here
+print "Welcome to the Calculator!"
+print "Type \"+ [number] [number]\" to add."
+print "Type \"- [number] [number]\" to subtract."
+print "Type \"* [number] [number]\" to multiply."
+print "Type \"/ [number] [number]\" to divide."
+print "Type \"square [number]\" to square the number."
+print "Type \"cube [number]\" to cube the number."
+print "Type \"pow [number] [number]\" to raise the first number to the power of the second number."
+print "Type \"% [number] [number]\" to get the remainer of the first number divided by the second number."
+print "Type \"q\" to exit the Calculator."
+
+
 while True:
     user_input = raw_input("> ")
     tokens = user_input.split(" ")
     if tokens[0] == "q":
-        quit()
+        break
     else:
         if tokens[0] == "+":
             print add(float(tokens[1]), float(tokens[2]))
@@ -30,3 +42,5 @@ while True:
             print power(float(tokens[1]), float(tokens[2]))
         elif tokens[0] == "%":
             print mod(float(tokens[1]), float(tokens[2]))
+        else:
+            print "Try again!  I don't recognize that math."
